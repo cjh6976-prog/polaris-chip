87fdcc217ffffbbe8896ec2fba410c779bf09451
cjh6976-prog <cjh6976@psu.edu>
2026-02-13 10:42:58 -0500

    Fancy Card Changes

--- src/my-card.js
+++ src/my-card.js
@@ -20,6 +20,8 @@ export class MyCard extends LitElement {
     this.paragraph = "Lebron James is widely considered one of the greatest basketball players of all time. Lebron has scored over 50,000 points in his career and has 4 NBA Championships. Along with that, he has also won league 4 MVPs.";
     this.buttonText = "Details";
     this.buttonLink = "https://hax.psu.edu";
+    this.fancy = false;
+   
 
   }
 
@@ -27,10 +29,11 @@ export class MyCard extends LitElement {
     return css`
     
     .Image{
-  width:200px;
+  max-width:200px;
+  height: 150px;
   margin: 16px auto;
   border-radius: 12px;
-  border: 5px solid var(--my-card-border-color, black);
+  border: 5px solid var(--my-card-border-color, yellow);
 }
 .Paragraph{
   text-align: center;
@@ -38,7 +41,7 @@ export class MyCard extends LitElement {
 }
 .Button{
   background-color: var(--my-card-button-color, yellow);
-  margin: 6px;
+  margin: auto;
   border: 4px solid var(--my-card-button-border-color, black);
   display: none;
 }
@@ -46,17 +49,30 @@ export class MyCard extends LitElement {
   background-color: var(--my-card-background-color, purple);
   padding: 12px;
   margin: 64px auto;
-  width: var(--my-card-width, 700px);
+  width: var(--my-card-width, 400px);
   text-align: center;
-  border: 5px solid var(--my-card-border-color, black);
+  border: 5px solid var(--my-card-border-color, yellow);
   border-radius: 12px;
   display: inline-block;
+  min-height: 400px;
 }
 .Heading {
   color: var(--my-card-text-color, yellow);
   font-size: 32px;
   text-decoration: underline;
 }
+.summary {
+  color: white;
+}
+details {
+  height: 100px;
+}
+details[open] .Paragraph {
+  max-height: 48px;
+  overflow-y: auto;
+  margin: auto 16px;
+}
+
 
 @media 
   (min-width: 500px) and
@@ -88,15 +104,41 @@ export class MyCard extends LitElement {
   
 
       :host {
-        display: block;
+        display: inline-block;
+        vertical-align: top;
 
         --my-card-background-color: purple;
         --my-card-text-color: yellow;
         --my-card-border-color: yellow;
         --my-card-width: 400px;
       }
+      :host([fancy]) .Card {
+  background-color: pink;
+  border-color: hotpink;
+}
+:host([fancy]) .Heading {
+  color: hotpink;
+}
+:host([fancy]) .Button {
+  background-color: hotpink;
+  border-color: white;
+}
+:host([fancy]) .Image {
+  border-color: hotpink;
+}
+:host([fancy]) .Paragraph {
+  color: hotpink; 
     `;
   }
+  openChanged(e) {
+  console.log(e);
+  if (e.target.getAttribute('open') !== null) {
+    this.fancy = true;
+  }
+  else {
+    this.fancy = false;
+  }
+}
 
   render() {
     return html`
@@ -104,7 +146,12 @@ export class MyCard extends LitElement {
 <div class="Card">
     <h1 class="Heading">${this.title}</h1>
     <img class="Image" src="${this.src}" alt="${this.alt}">
-<p class="Paragraph">${this.paragraph}</p>
+    <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+  <summary class="summary">Description</summary>
+  <div>
+<p class="Paragraph"><slot>${this.paragraph}</slot></p>
+</div>
+</details>
   <a href="${this.buttonLink}">
     
 <button class="Button">${this.buttonText}</button>
@@ -120,7 +167,8 @@ export class MyCard extends LitElement {
       src: { type: String },
       alt: { type: String },
       paragraph: { type: String },
-      buttonText: { type: String },
+      fancy: { type: Boolean, reflect: true },
+      buttonText: { type: String, attribute: 'button-text' },
       buttonLink: { type: String },
     };
   }
